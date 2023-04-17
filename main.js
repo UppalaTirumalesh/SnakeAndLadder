6b8fd9efbf1bcf0586934465365d7d8422aa96fe
uppalatirumalesh <uppalatirumalesh@gmail.com>
2023-04-17 19:40:01 +0530

    se

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function myFunction1(){
+    let x = Math.floor(Math.random() * 6) + 1 ;
+document.querySelector(".demo1").innerHTML = x;
+}
+function myFunction2(){
+    let x = Math.floor(Math.random() * 6) + 1 ;
+document.querySelector(".demo2").innerHTML = x;
+}
+
+
+
